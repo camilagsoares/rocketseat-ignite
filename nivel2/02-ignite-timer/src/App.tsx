475806749926640769ffb80ad9b12e18905b405d
camilagsoares <camila.soares19@hotmail.com>
2024-03-28 13:25:23 -0300

    .

--- nivel2/02-ignite-timer/src/App.tsx
+++ nivel2/02-ignite-timer/src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
 function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      REACT 
+      REACT
     </ThemeProvider>
   )
 }
