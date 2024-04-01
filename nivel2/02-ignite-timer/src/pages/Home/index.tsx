b84c693ad5ed421a62031b89438dd2929b8dbf0b
camilagsoares <camila.soares19@hotmail.com>
2024-04-01 15:37:59 -0300

    Estilizacoes home

--- nivel2/02-ignite-timer/src/pages/Home/index.tsx
+++ nivel2/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
 import { Play } from 'phosphor-react'
-import { HomeContainer, FormContainer, CountdownContainer } from './styles'
+import {
+  HomeContainer,
+  FormContainer,
+  CountdownContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
+} from './styles'
 
 export function Home() {
   return (
@@ -7,10 +15,14 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -18,15 +30,15 @@ export function Home() {
         <CountdownContainer>
           <span>0</span>
           <span>0</span>
-          <span>:</span>
+          <Separator>:</Separator>
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit" disabled>
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
