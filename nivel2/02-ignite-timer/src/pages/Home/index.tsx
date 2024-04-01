ea4aebe3574f86f8577b68e5181f55d533b23234
camilagsoares <camila.soares19@hotmail.com>
2024-04-01 15:46:33 -0300

    .

--- nivel2/02-ignite-timer/src/pages/Home/index.tsx
+++ nivel2/02-ignite-timer/src/pages/Home/index.tsx
@@ -15,13 +15,25 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-sugestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-sugestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5} // pula os números de cinco em cinco
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
